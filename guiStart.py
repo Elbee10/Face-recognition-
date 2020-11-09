18ee099c1217258652eb85234027c2bdf56a7e16
Munyema Jr <muhammadfb90@gmail.com>
2020-11-09 14:58:19 +0100

    Whole first page done

--- guiStart.py
+++ guiStart.py
@@ -6,54 +6,69 @@ connection = pymysql.connect(host="localhost",user="root",passwd="",database="at
 cursor = connection.cursor()
 
 
-
-
-
-
 if __name__ == "__main__":
     # create a GUI window
     gui = Tk()
-
     gui.configure(background='grey')
     gui.title('Student attendance')
-    gui.geometry('1800x700')
+    gui.geometry('900x700')
+
+    Grid.rowconfigure(gui, 0, weight=1)
+    Grid.columnconfigure(gui, 0, weight=1)
 
     topFrame = Frame(gui, bg='grey', width=400, height=10, pady=19, padx=19)
-    center = Frame(gui, bg='gray', width=400, height=10, padx=0, pady=0)
-    btm_frame = Frame(gui, bg='white', width=450, height=45, pady=3)
-    btm_frame2 = Frame(gui, bg='lavender', width=450, height=60, pady=3)
+    center = Frame(gui, bg='grey', width=400, height=10, padx=0, pady=0)
+    btm_frame2 = Frame(gui, bg='grey', width=450, height=60, pady=3)
 
-    gui.grid_rowconfigure(1, weight=1)
-    gui.grid_columnconfigure(0, weight=1)
 
-    topFrame.grid(row=0, sticky="ew")
-    center.grid(row=1, sticky="ew")
-    btm_frame.grid(row=3, sticky="ew")
-    btm_frame2.grid(row=4, sticky="ew")
+
+    topFrame.grid(row=0, sticky=N+S+E+W)
+    center.grid(row=1, sticky=N+S+E+W)
+    btm_frame2.grid(row=2, sticky=N+S+E+W)
+
 
 
     w = Label( topFrame, text="Welcome to the school face recognition attendance", font = "Verdana 20 bold", bg='grey')
+    copy = Label( btm_frame2, text="Copy right Munyema Jr. 2020", font = "Verdana12", bg='grey')
+    adminLabel = Label(center, text='Click for Administrator panel', font='Verdana12', bg='grey')
+    usernameLabel = Label( center, text="Username", font = "Verdana12", bg='grey')
+    passwordLabel = Label( center, text="Password", font = "Verdana12", bg='grey')
 
-    #center.grid_rowconfigure(0, weight=1)
-    #center.grid_rowconfigure(3, weight=1)
-    center.grid_columnconfigure(0, weight=2)
-    center.grid_columnconfigure(3, weight=2)
 
-    login = Button(center, text='Login')
-    Quit = Button(center, text='Quit', command=quit)
+    for i in range(2):
+        topFrame.grid_rowconfigure(i, weight=1)
+        topFrame.grid_columnconfigure(i, weight=1)
+        center.grid_columnconfigure(i, weight=1)
+    for i in range(5):
+        center.grid_rowconfigure(i, weight=1)
+        btm_frame2.grid_columnconfigure(0, weight=1)
+    btm_frame2.grid_rowconfigure(0, weight=1)
+
+
+
+    login = Button(center, text='Login', width=5, height=1)
+    admin = Button(center, text='Administrator')
+    passwordInput = Entry(center, show='*')
+    usernameInput = Entry(center)
+
+
     img = Image.open("/Users/alibarma/workspace/Face-recognition-/schoolLogo.jpg")
-    img = img.resize((1600, 300), Image.ANTIALIAS)
+    img = img.resize((1000, 300), Image.ANTIALIAS)
     img=ImageTk.PhotoImage(img)
     imgPanel = Label(topFrame, image = img)
 
-
-
-
-
-
     w.grid(row=0, columnspan=2)
-    login.grid(row=1, column=2, sticky='w')
-    Quit.grid(row=1, column=3)
+    copy.grid(row=0, column=4)
+
+    passwordLabel.grid(row=3,column=1)
+    usernameLabel.grid(row=1,column=1)
+    adminLabel.grid(row=1, column=0)
+
+    passwordInput.grid(row=4,column=1, pady=1)
+    usernameInput.grid(row=2,column=1, pady=1)
+
+    login.grid(row=5, column=1, pady=15)
+    admin.grid(row=2, column=0, pady=15)
     imgPanel.grid(row=1, column=1)
 
 
